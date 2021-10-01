36d6d7c1411507730650a145b08506ce90f3b8af
realAnand <realanand01@gmail.com>
2021-10-01 15:25:07 -0700

    Linkedlist implementation in java

--- LinkedList Implementation/LinkedList.java
+++ LinkedList Implementation/LinkedList.java
@@ -6,3 +6,11 @@ class Node{
 		this.next = null;
 	}
 }
+class main{
+	public static void main(String args[]){
+		Node n1 = new Node(10);
+		n1.next = new Node(15);
+		n1.next.next = new Node(20);
+		n1.next.next.next = new Node(25);
+	}
+}
