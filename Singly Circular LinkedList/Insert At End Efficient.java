a2949a2049e56c86f21cf7235836eccb525353c4
realAnand <realanand01@gmail.com>
2021-10-02 07:47:53 -0700

    Insert at end in singly circular linkedlist efficient approach

---
+++ Singly Circular LinkedList/Insert At End Efficient.java
@@ -0,0 +1,15 @@
+public static Node insertAtEndEfficient(Node head,int data){
+	Node temp = new Node(data);
+	if(head == null){
+		temp.next = temp;
+		return temp;
+	}
+	else{
+		temp.next = head.next;
+		head.next = temp;
+		int t = head.data;
+		head.data = temp.data;
+		temp.data = t;
+		return temp;
+	}
+}
