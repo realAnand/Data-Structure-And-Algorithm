ecc0e5bc4d5adc471b4a97aef2410684e2ff5e62
realAnand <realanand01@gmail.com>
2021-10-01 20:33:38 -0700

    Insert at given position in singly linkedlist

---
+++ LinkedList/Insert At Given Position.java
@@ -0,0 +1,17 @@
+public static Node insertAtPosition(Node head, int pos, int data){
+	Node temp = new Node(data);
+	if(pos == 1){
+		temp.next = head;
+		return temp;
+	}
+	Node curr = head;
+	for(int i=0; i<pos-2 && curr != null; i++){
+		curr = curr.next;
+	}
+	if(curr == null){
+		return head;
+	}
+	temp.next = curr.next;
+	curr.next = temp;
+	return head;
+}
