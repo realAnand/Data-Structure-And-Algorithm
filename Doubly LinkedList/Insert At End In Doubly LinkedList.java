d8db56ba466ed61e9f40bc48e4673b73d7e6d10d
realAnand <realanand01@gmail.com>
2021-10-02 00:33:47 -0700

    Insert at end in doubly linkedlist

---
+++ Doubly LinkedList/Insert At End In Doubly LinkedList.java
@@ -0,0 +1,13 @@
+public static Node insertAtEnd(Node head,int data){
+	Node temp = new Node(data);
+	if(head == null){
+		return temp;
+	}
+	Node curr = head;
+	while(curr.next != null){
+		curr = curr.next;
+	}
+	curr.next = temp;
+	temp.prev = curr;
+	return head;
+}
